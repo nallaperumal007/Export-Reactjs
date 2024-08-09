199c20b6e5113d6a0bb5259f28cc5dd68963d94d
nallaperumal007 <nallaperumal342@gmail.com>
2024-08-09 08:06:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CreateForm from './CreateForm';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <CreateForm />
+    
+    </div>
+  );
+};
+
+export default App;
